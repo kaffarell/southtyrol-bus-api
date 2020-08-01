bca1e4ee269978bf4245e5b2ee2beab2d896ab18
gabriel <gabrielgoller123@gmail.com>
2020-08-01 18:20:47 +0200

    🚨 Wrote single response test

--- __tests__/stopfinder_request.test.ts
+++ __tests__/stopfinder_request.test.ts
@@ -3,7 +3,18 @@ import { findLocationAction, stopFinderRequest } from '../src/controller/stopfin
 
 describe('Testing stopFinder', () => {
     test('Single stop', () => {
-
+        findLocationAction('11.68880', '46.57855')
+            .then((stop: Array<stopFinderRequest> | string) => {
+                if(typeof stop !== 'string'){
+                    expect(stop[0].nearestStopName).toEqual('Martin');
+                    //expect(stops[0].stopId).toEqual('234');
+                    expect(stop[0].nearestStopPlace).toEqual('St. Ulrich');
+                    expect(stop[0].nearestStopDistance).toEqual('909');
+                    expect(stop[0].nearestStopDistanceTime).toEqual('13');
+                    expect(stop[0].nearestPlace).toEqual('St. Ulrich');
+                    expect(stop[0].nearestStreet).toEqual('Streda Cuca 68');
+                }
+            });
     });
 
     test('Multiple stops', () => {
