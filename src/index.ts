18b837a6aec0218b95665fbcd6d0eabec9a4f9fb
gabriel <gabrielgoller123@gmail.com>
2020-08-05 08:39:18 +0200

    🐛 Added Port variable for environment port

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,15 @@
 import express from 'express';
 import api from './routes/api';
 
-
 const app = express();
+
+const port = process.env.PORT || 5000;
 
 app.use(express.json());
 
 app.use('/api', api);
 
 
-app.listen(5000, () => {
-    console.log('Server listening on Port 5000');
+app.listen(port, () => {
+    console.log('Server listening on Port ' + port);
 })
