feb9dcada10f156b449b62bb02391c90e86c2f28
kaffarell <gabrielgoller123@gmail.com>
2022-02-01 12:41:01 +0100

    :test_tube: Added dm_reqeust mock tests

---
+++ __tests__/dm_request.test.ts
@@ -0,0 +1,8 @@
+import { dmRequestAction, IDmRequest } from '../src/controller/dm_request';
+
+
+describe.skip('Testing dm request', () => {
+    test('Testing dm_request', (done) => {
+
+    });
+});
