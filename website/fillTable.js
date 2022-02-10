02b221273269ee96572a2f6e4e19e2419a80bee5
kaffarell <gabrielgoller123@gmail.com>
2022-02-10 10:08:19 +0100

    :ambulance: Forgot to add js files
    huch

---
+++ website/fillTable.js
@@ -0,0 +1,46 @@
+tableBx = document.getElementById("abfahrt-bx");
+tableKh = document.getElementById("abfahrt-kh");
+
+
+dmRequestAction("66001143").then(data => {
+    let tbodyBX = document.createElement('tbody');
+    let tbodyKH = document.createElement('tbody');
+
+        for(let j = 0; j<data.length; j++){
+            let trBX = document.createElement('tr'); //spalten
+            let trKH = document.createElement('tr');
+            
+            let parsedData = parseData(data, j);
+            for(let i = 0; i<4;i++){
+                let td = document.createElement('td');
+                td.innerText = parsedData[i];
+                console.log("Richtung: " + parsedData[4]);
+                if(parsedData[4] == "H")
+                    trKH.appendChild(td);
+                else
+                    trBX.appendChild(td);
+            }
+            if(trBX.innerHTML.length !== 0)
+                tbodyBX.appendChild(trBX);
+            if(trKH.innerHTML.length !== 0)
+                tbodyKH.appendChild(trKH);
+        }
+    tableBx.appendChild(tbodyBX);
+    tableKh.appendChild(tbodyKH);
+});
+
+function parseData(data, index){
+    console.log(data);
+    let arr = [];
+    arr.push(data[index].number);
+    arr.push(data[index].directionName);
+    if(data[index].timeMinute.length == 1)
+        arr.push(data[index].timeHour + ":" + "0" + data[index].timeMinute);
+    else
+        arr.push(data[index].timeHour + ":" + data[index].timeMinute);
+    arr.push(data[index].realtime);
+    arr.push(data[index].direction)
+    console.log(arr);
+    return arr;
+
+}
---
+++ website/reddit.js
@@ -0,0 +1,59 @@
+function extractUrlFromJson(returnBody){
+    try{
+        const usefulResponse = JSON.parse(returnBody).data.children[0].data.preview.images[0];
+        
+        if(usefulResponse == null) {
+            console.error('No useful data found in response');
+            return "";
+        }
+
+        // If there is a gif, return it, otherwise return the image
+        if(usefulResponse.variants?.gif?.source?.url !== undefined) {
+            console.log("gif");
+            return usefulResponse.variants.gif.source.url;
+        }else {
+            console.log("image");
+            return usefulResponse.source.url;
+        }
+
+    }catch(e) {
+        console.error(e);
+    }
+}
+
+async function getSubredditData() {
+    try{
+        let headers = new Headers({
+            'User-Agent': 'MockClient/0.1 by Me'
+        });
+        let response = await fetch('https://api.reddit.com/r/memes/top?t=day&raw_json=1', {
+            headers: headers
+        });
+        return response.text();
+    }catch(e) {
+        console.error('Error when fetching data');
+        console.error(e);
+        throw(e);
+    }
+}
+
+async function memeOfTheDayAction() {
+    let jsonData = '';
+
+    try {
+        jsonData = await getSubredditData();
+    } catch (error) {
+        console.error('Encountered error when fetching data');
+        console.error(error);
+        return error;
+    }
+    let processedData = '';
+    try {
+        processedData = extractUrlFromJson(jsonData);
+    } catch (error) {
+        console.error('Encountered error when extracting data from response');
+        console.error(error);
+        return error;
+    }
+    return processedData;
+}
